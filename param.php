aa848c754f7edb88a185d529d7e7985c7c25e45e
IGORPOGODAEV <program.igor@yandex.ru>
2017-12-20 19:15:40 +0300

    second commit
    It simply start

--- param.php
+++ param.php
@@ -5,4 +5,4 @@ define("app",ROOT_FORDER . "/application");
 define("lib",ROOT_FORDER . "/library");
 define("view",ROOT_FORDER . "view");
 
-include  app."function/dump.php";
+include  app."/function/dump.php";
